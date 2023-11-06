b6780c0e5bcad234f2d1e462b7201259b940c443
rowanspain <125788793+rowanspain@users.noreply.github.com>
2023-11-05 23:05:42 -0800

    LessonD

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -7,6 +7,8 @@ public class Enemy : MonoBehaviour
     public float speed = 4.0f;
     Rigidbody enemyRB;
     GameObject player;
+
+    private float yBoundary = -15.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,6 +19,10 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(transform.position.y < yBoundary)
+        {
+            Destroy(gameObject);
+        }
         Vector3 seekDirection = (player.transform.position - transform.position).normalized;
         enemyRB.AddForce(seekDirection * speed * Time.deltaTime);
     }
--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -5,11 +5,36 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject enemyPrefab;
+    public GameObject powerUpPrefab;
+   
     private float spawnRange = 8.5f;
+    private int enemyCount;
+    private int waveNumber = 1;
+
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+        SpawnWave(waveNumber);
+    }
+
+
+    private void Update()
+    {
+        enemyCount = FindObjectsOfType<Enemy>().Length;
+        if(enemyCount == 0)
+        {
+            waveNumber++;
+            SpawnWave(waveNumber);
+        }
+    }
+
+    void SpawnWave(int enemyNum)
+    {
+        Instantiate(powerUpPrefab, GenerateSpawnPosition(), powerUpPrefab.transform.rotation);
+        for (int i = 0; i < enemyNum; i++)
+        {
+            Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+        }
     }
 
     Vector3 GenerateSpawnPosition()
@@ -18,11 +43,5 @@ public class SpawnManager : MonoBehaviour
         float zPos = Random.Range(-spawnRange, spawnRange);
         Vector3 spawnPos = new Vector3(xPos, enemyPrefab.transform.position.y, zPos);
         return spawnPos;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
     }
 }
